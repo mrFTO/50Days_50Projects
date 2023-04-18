7ff487aaed7170e5159dce63c24a8db2d508c016
mrFTO <ntoon@ya.ru>
2023-04-19 00:11:04 +0300

    fix: fixed few bugs and dom work with vpn

--- Day 17 project/script.js
+++ Day 17 project/script.js
@@ -28,7 +28,7 @@ function showMovies(movies) {
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <div class="movie">
+        
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
             <h3>${title}</h3>
@@ -38,7 +38,7 @@ function showMovies(movies) {
             <h3>Overview</h3>
             ${overview}
             </div>
-        </div>
+        
         `
 
         main.appendChild(movieEl)
